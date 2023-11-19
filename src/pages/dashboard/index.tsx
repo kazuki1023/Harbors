03e08f17d4a4df9d614f4abf5bbce67ffc8413bd
kazuki1023 <k.keio12569@gmail.com>
2023-11-19 12:36:03 +0900

    first commit

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+import { NextPage } from 'next';
+
+const Dashboard: FC = () => {
+  return (
+    <div>dashboard</div>
+  )
+}
+
+const Page: NextPage = () => {
+  return <Dashboard />;
+};
+
+export default Page;
+
