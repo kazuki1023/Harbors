edd000d8e99a624550f12bcec29e670e817a7b91
kazuki1023 <k.keio12569@gmail.com>
2023-12-17 02:03:32 +0900

    作成したlayoutsをcomponent内で呼び出した

---
+++ src/components/pages/dashboard/index.tsx
@@ -0,0 +1,11 @@
+import  BaseLayout  from '@/layouts/BaseLayout';
+
+const Dashboard = ()=> {
+  return (
+    <BaseLayout>
+      <div>Dashboard</div>
+    </BaseLayout>
+  )
+}
+
+export default Dashboard;
