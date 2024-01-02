6a3be43b1f1c7882b1f3fd798f544271d01dffdc
kazuki1023 <k.keio12569@gmail.com>
2024-01-03 02:38:59 +0900

    HeaderLogoをdashboardにimport

--- src/components/pages/dashboard/index.tsx
+++ src/components/pages/dashboard/index.tsx
@@ -1,9 +1,11 @@
 import  BaseLayout  from '@/layouts/BaseLayout';
+import HeaderLogo from '@/components/organisms/HeadLogo';
 
 const Dashboard = ()=> {
   return (
     <BaseLayout>
       <div>Dashboard</div>
+      <HeaderLogo />
     </BaseLayout>
   )
 }
