59c7427fc104f6103d259d8acccbcfb38848c7e3
kazuki1023 <k.keio12569@gmail.com>
2024-01-03 03:13:17 +0900

    axpect比で高さを調整

--- src/components/organisms/HeadLogo/index.tsx
+++ src/components/organisms/HeadLogo/index.tsx
@@ -2,7 +2,7 @@ import 'tailwindcss/tailwind.css'
 import Image from 'next/image';
 const HeaderLogo = () => {
   return (
-    <div className="relative w-full h-screen" >
+    <div className="relative w-full aspect-w-2 aspect-h-1" >
       <Image src="/harbors_top.jpg" alt="harbor" layout="fill"
         objectFit="contain" />
     </div>
