c42948b769a6702a8ad96ad313eb9e040135ae9a
kazuki1023 <k.keio12569@gmail.com>
2024-01-03 21:44:29 +0900

    ログインボタンを作成

---
+++ src/components/organisms/LineButton/index.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+const LineButton = () => {
+  return (
+    <button className="bg-[#00c300] text-white px-4 py-2 rounded-full flex items-center justify-center gap-2">
+      <span>
+        <Image
+          src="/btn_base.png"
+          alt="line"
+          width={20}
+          height={20}
+          style={{
+            maxWidth: "100%",
+            height: "auto"
+          }} />
+      </span>
+      <span>Log in</span>
+    </button>
+  );
+}
+export default LineButton;
--- src/components/pages/account/index.tsx
+++ src/components/pages/account/index.tsx
@@ -1,11 +1,13 @@
 import  BaseLayout  from '@/layouts/BaseLayout';
 import HeaderLogo from '@/components/organisms/HeadLogo';
 import { CheckboxSection } from '@/components/organisms/CheckboxSection';
+import LineButton from '@/components/organisms/LineButton';
 
 const Account = ()=> {
   return (
     <BaseLayout>
       <div>Account</div>
+      <LineButton />
     </BaseLayout>
   )
 }
