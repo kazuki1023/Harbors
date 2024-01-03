7b8858ce3a7f287e80e6ef45de6f9d7f00052299
kazuki1023 <k.keio12569@gmail.com>
2024-01-04 01:57:39 +0900

    作成したapiを叩いてlineログイン成功

--- src/components/pages/dashboard/index.tsx
+++ src/components/pages/dashboard/index.tsx
@@ -1,8 +1,45 @@
-import  BaseLayout  from '@/layouts/BaseLayout';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import BaseLayout from '@/layouts/BaseLayout';
 import HeaderLogo from '@/components/organisms/HeadLogo';
 import { CheckboxSection } from '@/components/organisms/CheckboxSection';
 
-const Dashboard = ()=> {
+const Dashboard = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const { code } = router.query;
+    if (code) {
+      fetch('/api/getToken', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ code: code })
+      })
+        .then(response => response.json())
+        .then(data => {
+          if (data.access_token) {
+            fetch('/api/fetchProfile', {
+              method: 'GET',
+              headers: {
+                'Authorization': `Bearer ${data.access_token}`
+              },
+            })
+              .then(response => response.json())
+              .then(profileData => {
+                console.log(profileData);
+              })
+              .catch(error => {
+                console.error('トークン検証中にエラーが発生しました', error);
+              });
+          }
+        })
+        .catch(error => {
+          console.error('トークン交換中にエラーが発生しました', error);
+        });
+    }
+  }, [router.query]);
   return (
     <BaseLayout>
       <div>Dashboard</div>
