6bf9b0795a63a79bd76730d883a6dc1d538527e4
kazuki1023 <k.keio12569@gmail.com>
2024-01-03 18:04:29 +0900

    imageの指定をnext14に合わせて変更した

--- src/components/organisms/HeadLogo/index.tsx
+++ src/components/organisms/HeadLogo/index.tsx
@@ -1,11 +1,17 @@
 import 'tailwindcss/tailwind.css'
-import Image from "next/legacy/image";
+import Image from "next/image";
 const HeaderLogo = () => {
   return (
     <div className="relative w-full aspect-w-2 aspect-h-1" >
-      <Image src="/harbors_top.jpg" alt="harbor" layout="fill"
-        objectFit="contain" />
+      <Image
+        src="/harbors_top.jpg"
+        alt="harbor"
+        fill
+        sizes="100vw"
+        style={{
+          objectFit: "contain"
+        }} />
     </div>
-  )
+  );
 }
 export default HeaderLogo;
