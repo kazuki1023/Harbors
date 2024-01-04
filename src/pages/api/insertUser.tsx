70a2534bc232cf08de00419a302988d2bd236232
kazuki1023 <k.keio12569@gmail.com>
2024-01-04 22:59:21 +0900

    登録するユーザーの情報をid_tokenに変えた

--- src/pages/api/insertUser.tsx
+++ src/pages/api/insertUser.tsx
@@ -1,17 +1,17 @@
 import { sql } from '@vercel/postgres';
 import { NextApiRequest, NextApiResponse } from 'next';
-export default async function handler(req:NextApiRequest , res:NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     try {
       const { userId, name, picture } = req.body;
       // データベースにユーザー情報を挿入する
       await sql`
-        INSERT INTO users (userId, name, picture)
+        INSERT INTO users (id_token, name, picture)
         VALUES (${userId}, ${name}, ${picture})
-        ON CONFLICT (userId) DO NOTHING;
+        ON CONFLICT (id_token) DO NOTHING
       `;
 
-      res.status(200).json({ message: 'ユーザー情報を挿入しました。' });
+      res.status(200).json({ message: '新しいユーザーを挿入しました。' });
     } catch (error) {
       console.error('Error inserting user:', error);
       res.status(500).json({ message: 'ユーザー情報の挿入に失敗しました。' });
