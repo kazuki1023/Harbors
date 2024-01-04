1cccae34b8552aaf6c38f1577c604f22af703cd5
kazuki1023 <k.keio12569@gmail.com>
2024-01-04 12:03:53 +0900

    不要コメントを削除し、urlの読み込み方法を変えた

--- src/components/organisms/LineButton/container.tsx
+++ src/components/organisms/LineButton/container.tsx
@@ -7,13 +7,11 @@ export const LineButtonContainer = () => {
     const nonce = generateRandomString();
 
     const clientId = process.env.NEXT_PUBLIC_LINE_CLIENT_ID;
-    console.log(clientId);
-    const redirectUri = encodeURI("https://harbors.vercel.app/dashboard")
-    console.log(redirectUri);
+    const redirectUri = process.env.NEXT_PUBLIC_LINE_REDIRECT_URI ? process.env.NEXT_PUBLIC_LINE_REDIRECT_URI : 'https://harbors.vercel.app/dashboard';
     const scope = 'profile%20openid';
 
     const url = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}&scope=${scope}&nonce=${nonce}`;
-    window.location.href = url; // ユーザーを認証URLにリダイレクトする
+    window.location.href = url;
   };
   return (
     <LineButtonPresentation onClick={handleLogin}/>
