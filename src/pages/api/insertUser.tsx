8c0b021206594e91aae165954af3f4c271b90c09
kazuki1023 <k.keio12569@gmail.com>
2024-01-07 00:11:35 +0900

    idTokenが毎回違って重複カウントするので、pictureで重複チェックする

--- src/pages/api/insertUser.tsx
+++ src/pages/api/insertUser.tsx
@@ -4,11 +4,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method === 'POST') {
     try {
       const { userId, name, picture } = req.body;
+      console.log(req.body)
       // データベースにユーザー情報を挿入する
+      // id_tokenは毎回違うのでおなじーざーでも複数登録されるので、写真で判定する
       await sql`
         INSERT INTO users (id_token, name, picture)
         VALUES (${userId}, ${name}, ${picture})
-        ON CONFLICT (id_token) DO NOTHING
+        ON CONFLICT (picture) DO NOTHING
       `;
 
       res.status(200).json({ message: '新しいユーザーを挿入しました。' });
