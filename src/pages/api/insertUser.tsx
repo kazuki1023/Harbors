6f4d2f47dc751fb958508057032641ad610e3b6c
kazuki1023 <k.keio12569@gmail.com>
2024-01-07 16:55:54 +0900

    id_tokenカラムで重複チェックかけた

--- src/pages/api/insertUser.tsx
+++ src/pages/api/insertUser.tsx
@@ -4,11 +4,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method === 'POST') {
     try {
       const { userId, name, picture } = req.body;
-      console.log(req.body)
       // データベースにユーザー情報を挿入する
       await sql`
         INSERT INTO users (id_token, name, picture)
         VALUES (${userId}, ${name}, ${picture})
+        on conflict (id_token) do nothing
       `;
 
       res.status(200).json({ message: '新しいユーザーを挿入しました。' });
