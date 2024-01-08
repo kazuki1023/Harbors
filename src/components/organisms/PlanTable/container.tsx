00edcd6f0830cf6e2711562dcc9fdc7ff8602421
kazuki1023 <k.keio12569@gmail.com>
2024-01-08 13:08:01 +0900

    取得したデータを加工した

--- src/components/organisms/PlanTable/container.tsx
+++ src/components/organisms/PlanTable/container.tsx
@@ -4,8 +4,25 @@ import { userTask } from '@/types/UserTask';
 interface PlanTableContainerProps {
     userTask: userTask[];
 }
-export const PlanTableContainer: React.FC<PlanTableContainerProps> = ({userTask}) => {
+export const PlanTableContainer: React.FC<PlanTableContainerProps> = ({ userTask }) => {
+    const transformedData: any = {};
+
+    userTask.forEach(item => {
+        if (!transformedData[item.userid]) {
+            transformedData[item.userid] = {
+                userID: item.userid,
+                picture: item.picture,
+                displayName: item.name,
+                tasks: []
+            };
+        }
+        transformedData[item.userid].tasks.push(item.task);
+    });
+
+    const taskGroupedByUser = Object.values(transformedData);
+    console.log(taskGroupedByUser);
+
     return (
-        <PlanTablePresentation userTask={userTask}/>
+        <PlanTablePresentation userTask={userTask} />
     )
 }
